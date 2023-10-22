e9c21a3099fb80399d7236393dbfb77e97a92ee9
unknown <pradyumn.sk@gmail.com>
2023-10-22 19:09:40 +0530

    Add New Component About and Dark Mode in About Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="PradTextUtils" aboutText="about us" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to anlyze Below" />
+        {/* <TextForm heading="Enter the text to anlyze Below" /> */}
+        <About />
       </div>
     </>
   );
