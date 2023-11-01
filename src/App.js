0455dbfbf4fcb84cf6d4407c262f3477a91d85d9
unknown <pradyumn.sk@gmail.com>
2023-11-01 10:35:30 +0530

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); //Where Dark Mode Is Enable Or Not
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#021030";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
     <>
-      <Navbar title="PradTextUtils" aboutText="about us" />
-      <div className="container my-3">
-        {/* <TextForm heading="Enter the text to anlyze Below" /> */}
-        <About />
-      </div>
+      <Router>
+        <Navbar
+          title="PradTextUtils"
+          aboutText="about us"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to anlyze Below" mode={mode}  />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,16 +16,28 @@ export default function TextForm(props) {
     setText(nText);
   };
   const handelCopyClick = () => {
-    let nText = navigator.clipboard.writeText(text);
-    alert('Content copied to clipboard' + nText);
+    navigator.clipboard.writeText(text);
+    alert('Content copied to clipboard');
   };
+
+  const handelSpace = () => {
+    let nText = text.replace(/\s+/g, ' ');
+    setText(nText);
+  };
+
+  const handelCaptalize = () => {
+    let nText = text.charAt(0).toUpperCase() + text.slice(1);
+    setText(nText);
+  };
+
+
   const handelOnChange = (event) => {
     setText(event.target.value);
   };
   const [text, setText] = useState("");
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color : props.mode==='dark'? 'white' : '#021030'}}>
         <h1 className="mb-3">{props.heading}</h1>
         <div className="mb-3">
           <textarea
@@ -34,19 +46,22 @@ export default function TextForm(props) {
             onChange={handelOnChange}
             value={text}
             rows="8"
+            style={{backgroundColor : props.mode==='dark' ? '#021030' : 'white', color : props.mode==='dark'? 'white' : 'black'}}
           ></textarea>
         </div>
         <button className="btn btn-outline-primary mx-1" onClick={handelUpClick}> Convert UpperCase </button>
         <button className="btn btn-outline-info mx-1" onClick={handelLoClick}> Convert UpperCase </button>
         <button className="btn btn-outline-danger mx-1" onClick={handelClearClick}> Clear </button>
         <button className="btn btn-outline-success mx-1" onClick={handelCopyClick}> Copy </button>
+        <button className="btn btn-outline-warning mx-1" onClick={handelSpace}> Remove Extra Space</button>
+        <button className={`btn btn-outline-${props.mode==='dark'? 'light' : 'dark'} mx-1`} onClick={handelCaptalize}>First Later Capitle</button>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{color : props.mode==='dark'? 'white' : '#021030'}}>
         <h2>Text Summary</h2>
         <p>{text.split(" ").length} Words and {text.length} Character</p>
         <p>{0.008 * text.split(" ").length} Minutes read.</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter something text in text box and then preview"}</p>
       </div>
     </>
   );
